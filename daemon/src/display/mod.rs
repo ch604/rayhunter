06b913e2aa00652a97fef7e6d5c1245eb4f659a7
Andrej Walilko <awalilko@liquidweb.com>
2026-02-15 18:37:06 -0500

    revert comment change

--- daemon/src/display/mod.rs
+++ daemon/src/display/mod.rs
@@ -20,8 +20,8 @@ pub enum DisplayState {
     /// We're not recording.
     Paused,
     /// A non-informational event has been detected.
-    //
-    // Note that EventType::Informational is never sent through this. If it is, it's the same as
-    // Recording
+    ///
+    /// Note that EventType::Informational is never sent through this. If it is, it's the same as
+    /// Recording
     WarningDetected { event_type: EventType },
 }
